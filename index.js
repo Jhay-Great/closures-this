a39567c3a50682ca7e5b42b0edb8f827f0427e81
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-19 10:34:24 +0000

    minor additions

--- index.js
+++ index.js
@@ -13,54 +13,43 @@
 const Person = {
     name: 'John',
     age: 43,
-
-    // greet() {
-    //     console.log(`Hello my name is ${this.name} and I'm ${this.age} years old`);
-    // }
+    greet() {
+        console.log(`Hello my name is ${this.name} and I'm ${this.age} years old`);
+    }
+    
 }
 
-// const felix = {
-//     name: 'felix',
-//     age: 23,
-// }
-
-// felix.bind(this)
-
+Person.greet();
 // console.log(Person);
-
-
-const greet = function() {
-    console.log(`Hello my name is ${this.name} and I'm ${this.age} years old`);
-}
-
-Person.greet = greet;
-// Person.greet();
-
-const Woman = {
-    name: 'Emilia', 
+const person2 = {
+    name: 'felix',
     age: 32,
-};
-Woman.greet = greet;
-// Woman.greet();
-
-
-// Using object constructor
-const Person1 = function(name, age) {
-    this.name = name;
-    this.age = age;
 }
+const person2Greet = Person.greet.bind(person2);
+person2Greet()
+
+// call
+const person3 = {
+    name: 'Joan',
+    age: 20,
+}
+Person.greet.call(person3);
+
+// apply
+const person4 = {
+    name: 'Manel',
+    age: 22,
+}
+Person.greet.apply(person4);
+/**Observation or response
+ * the call, apply and bind are methods which helps to change the value of the 'this' keyword. The bind method returns a function bound to the new object, then we can call that function to execute or run the method.
+ * The call method immediately calls the method with the new object bound to it, the call method does not return a function like the bind method. The call methods also calls the method with a list of variables after the this argument has been specified.
+ * The apply method is similar to the call method as it also calls the method immediately without returning a function like the bind method. The apply method also accepts an array of the variables which would be called with the apply method.
+ */
 
 
 
-
-
-const person1 = new Person1('kofi', 2)
-// console.log(person1);
-
-
-
-// 2. Event Handlers and this
-
+// 2. Event Handlers and this          
 const button = document.querySelector('button');
 // const handleClick = function() {
 //     console.log(this); // returns the element
@@ -74,17 +63,10 @@ const handleClick = () => {
 }
 button.addEventListener('click', handleClick)
 /**Evaluation or response
- * the 'this' keyword in a function declaration or anonymous function points or is a reference to the element calling the event. And in this cases that's the button element. Logging this to the console called in the function expression, it returned the element attached to the addEventlistener method, calling 'this' on the 'id' logs the value of the id passed as an attribute to the element. However when an arrow function is used the 'this' keyword points to the window object. This is clear in the example when 'this' was logged to the console. 'this.id' returned undefined since where was no property of 'id' on the window object. From this it's clear the calling 'this' in the addEventListener method would reference the window object whiles in the function declaration or anonymous function expression it refers to the element attached to the event listener.
+ * the 'this' keyword in a function declaration or anonymous function points or is a reference to the element calling the event. And in this cases that's the button element. Logging this to the console called in the function expression, it returned the element attached to the addEventlistener method, calling 'this' on the 'id' 'this.id' logs the value of the id passed as an attribute to the element. However when an arrow function is used the 'this' keyword points to the window object. This is clear in the example when 'this' was logged to the console. 'this.id' returned undefined since where was no property of 'id' on the window object. From this it's clear the calling 'this' in the addEventListener method would reference the window object whiles in the function declaration or anonymous function expression it refers to the element attached to the event listener.
  */
 
-// 3.Private Data with Closures and this:
-// • Create a function createCounter() that:
-// o Has a private variable count initialized to 0.
-// o Returns an object with two methods:
-// 1. increment(): Increments the count and logs the new value
-// to the console using this.count.
-// 2. getCount(): Returns the current value of count.
-
+// 3.Private Data with Closures and this
 const createCounter = function() {
     let count = 0;
     return {
@@ -104,40 +86,34 @@ const createCounter = function() {
 // const counter = createCounter();
 // counter.increment();
 // counter.increment();
-// counter.increment();
-// counter.increment();
 // counter.getCount();
 
 
-// 4. Reusable Component with Closure and this.
-// Create a function createTimer(duration, elementId) that:
-// Takes a duration in seconds and an elementId as input.
-// Starts a timer that counts down from duration to 0.
-
-// Updates the content of the element with the given elementId to
-// display the remaining time every second.
-// When the timer reaches 0, logs a message to the console.
-// Uses closures to store the timer’s state (remaining time) and this to refer to the correct element.
-
+// 4. Reusable Component with Closure and this
 const createTimer = function(duration, elementId) {
     // initializing state and element reference
-    let time = duration; // private value
+    let time = duration; 
     const element = document.getElementById(elementId);
+    console.log(this)
 
     const timer = () => {
+        console.log(this);
         element.textContent = time;
         time -= 1;
         if (time <  0) {
             console.log('Your time is up!!!!');
-            // removeSetInterval(timer);
             clearInterval(interval);
-
         }
     }
     // set the timer
-    const interval = setInterval(timer, 1000)
+    console.log(this);
+    const countDown = timer.bind(this);
+    const interval = setInterval(countDown, 1000)
 }
-createTimer(10, 'display');
+// createTimer(10, 'display');
+/**Observation
+ * the 'this' keyword in this instance returns undefined 'in strict mode' but without 'use strict' statement, it references the window object since createTimer being called as a standalone function. If the 'createTimer' function was called as a method of an object's property the 'this' keyword would reference the object the 'createTimer' method is attached to.
+ */
 
 
 
